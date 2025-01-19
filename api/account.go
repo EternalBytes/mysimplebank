6346fd4cca1767835f412853021556d453ace00d
Maycon Paranhos <maycon.rodrigues1@gmail.com>
2025-01-19 17:18:57 -0300

    add and implement getAccount and  listAccount

--- api/account.go
+++ api/account.go
@@ -59,3 +59,29 @@ func (server *Server) getAccount(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, &account)
 }
+
+type listAccountRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listAccount(ctx *gin.Context) {
+	var req listAccountRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorMessage(err))
+		return
+	}
+
+	args := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, args)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorMessage(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+}
