c0dba04794c8e4cc197a693e828d8aff91db2c43
Maycon Paranhos <maycon.rodrigues1@gmail.com>
2025-01-19 23:28:25 -0300

    config and use Viper to load config file or environment variable

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,12 +7,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/eternalbytes/simplebank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver string = "postgres"
-	dbSource string = "postgresql://ROOT:secret@localhost:5432/mysimple-bank?sslmode=disable"
 )
 
 var testQueries *Queries
@@ -20,8 +16,11 @@ var conn *sql.DB
 var ctx = context.Background()
 
 func TestMain(m *testing.M) {
-	var err error
-	conn, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../../")
+	if err != nil {
+		log.Fatalln("cannot load config:", err)
+	}
+	conn, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatalln("can't connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/eternalbytes/simplebank/api"
 	db "github.com/eternalbytes/simplebank/db/sqlc"
+	"github.com/eternalbytes/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      string = "postgres"
-	dbSource      string = "postgresql://ROOT:secret@localhost:5432/mysimple-bank?sslmode=disable"
-	serverAddress string = "localhost:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatalln("cannot load cofigurations", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatalln("can't connect to db:", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatalln("cannot start server", err)
 	}
